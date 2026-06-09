3b7bf7c975f529d7bd5ef27685cd52ae129dc38e
Chiboy16 <ugwuchibuike15@gmail.com>
2026-06-09 13:29:52 +0100

    Fixed quotation tokens and missing object commas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require(”dotenv").config();
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
@@ -259,8 +259,8 @@ bot.on("message", (msg) => {
             amount: amt,
             currency: "NGN",
             reference: ref,
-            notification_url: "https://berry-burger-store.onrender.com/api/payment-webhook"
-            redirect_url: "https://t.me/Berryburgersmsbot",
+            notification_url: "https://berry-burger-store.onrender.com/api/payment-webhook",           
+            redirect_url: "https://t.me/berryburgersmsbot",
             customer: {
                 email: chatId + "@telegram.com",
                 name: msg.from.first_name || "Telegram User"
